f80d62393316d356851337e756245cf98c95d6f7
Princegupta <125169311+Princegupta101@users.noreply.github.com>
2023-06-26 17:06:59 +0530

    Mock-1

---
+++ Mock-1/Q2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+	public static int fristChar(String s){
+	     int[] count = new int[26];
+        for(int i=0; i<s.length();i++)
+        {
+          count[s.charAt(i) -'a']++;
+          }
+     for(int i=0; i<s.length();i++)
+        {
+          if(count[s.charAt(i) -'a']==1){
+            return i;
+              
+              }    
+          }   
+	    return -1;
+	}
+	public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+          System.out.println(fristChar(s));   
+    }
+    
+}
+￼Enter
